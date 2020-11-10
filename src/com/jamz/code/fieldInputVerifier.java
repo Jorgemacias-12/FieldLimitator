034d191aa1d5423a79ea31c56d661d841b1fb32e
Jorgemacias-12 <jmacias3118@jal.conalep.edu.mx>
2020-11-09 22:06:46 -0600

    Funciones estáticas terminadas, las pruebas de funcionamiento han funcionado correctamente.

--- src/com/jamz/code/fieldInputVerifier.java
+++ src/com/jamz/code/fieldInputVerifier.java
@@ -1,5 +1,6 @@
 package com.jamz.code;
 
+import java.awt.Toolkit;
 import java.awt.event.KeyEvent;
 import javax.swing.JTextField;
 
@@ -17,7 +18,7 @@ public class fieldInputVerifier
 {
     public final static int NO_CHARACTERS = 0;
     public final static int NO_NUMBERS = 1;
-    public final static int LETTERS_WO_SPECIAL_CHARACTERS = 2;
+    public final static int CHARACTERS_WO_SPECIAL_CHARACTERS = 2;
     public final static int NUMBERS_WO_SPECIAL_CHARACTERS = 3;
    
     private final static char[] specialCharacters = 
@@ -29,19 +30,92 @@ public class fieldInputVerifier
     
     private static boolean argumentError = false;
    
-    public static void setInputLimit(int inputFilter, KeyEvent evt)
+    public static void setInputLimit(JTextField field,
+                                      int characterLimit,
+                                      KeyEvent evt) 
     {
-        switch(inputFilter)
+        if(characterLimit <= 0)
         {
-            
+            if(!argumentError)
+            {
+                argumentError = true;
+                System.err.println("ERROR: The argument must be higher than 0,"
+                + " value introduced: "+characterLimit);
+            }
+        }
+        else
+        {
+            if(field.getText().length() >= characterLimit)
+            {
+                evt.consume();
+                Toolkit.getDefaultToolkit().beep();
+            }
         }
     }
     
-    public static void setInputFilter(JTextField field,
-                                      int characterLimit,
-                                      KeyEvent evt)
+    public static void setInputFilter(int inputFilter, KeyEvent evt)
     {
-        
+        switch(inputFilter)
+        {
+            case NO_CHARACTERS:
+                if(Character.isLetter(evt.getKeyChar()))
+                {
+                    evt.consume();
+                    Toolkit.getDefaultToolkit().beep();
+                }
+                break;
+            case NO_NUMBERS:
+                if(Character.isDigit(evt.getKeyChar()))
+                {
+                    evt.consume();
+                    Toolkit.getDefaultToolkit().beep();
+                }
+                break;
+            case CHARACTERS_WO_SPECIAL_CHARACTERS:
+                if(Character.isDigit(evt.getKeyChar()))
+                {
+                    evt.consume();
+                    Toolkit.getDefaultToolkit().beep();
+                }
+                else
+                {
+                    for( char currentCharacter : specialCharacters)
+                    {
+                        if(currentCharacter == evt.getKeyChar())
+                        {
+                            evt.consume();
+                            Toolkit.getDefaultToolkit().beep();
+                        }
+                    }
+                }
+                break;
+            case NUMBERS_WO_SPECIAL_CHARACTERS:
+                if(Character.isLetter(evt.getKeyChar()))
+                {
+                    evt.consume();
+                    Toolkit.getDefaultToolkit().beep();
+                }
+                else
+                {
+                    for( char currentCharacter : specialCharacters)
+                    {
+                        if(currentCharacter == evt.getKeyChar())
+                        {
+                            evt.consume();
+                            Toolkit.getDefaultToolkit().beep();
+                        }
+                    }
+                }
+                break;
+            default:
+                if(!argumentError)
+                {
+                    argumentError = true;
+                    throw new 
+                    IllegalArgumentException("The argument of this function"
+                    + "must be valid");
+                }
+        }
     }
     
 }
